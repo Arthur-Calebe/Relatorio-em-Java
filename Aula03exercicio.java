5cec5ae87bda83af8351dab49393ef84755d38fb
Arthur Calebe <arthurcarvalhodas@gmail.com>
2025-05-10 15:59:30 -0300

    Add files via upload

---
+++ Aula03exercicio.java
@@ -0,0 +1,15 @@
+package academy.devdojo.maratonajava.introdução;
+
+public class Aula03exercicio {
+    public static void main(String[] args) {
+        int idade = 16;
+        double salarioDouble = 5500;
+        String endereçoString = "Vila da folha";
+        String nomeCompleto = "Naruto Uzumaki";
+        String dataRecebimento = "04 de Maio de 2007";
+
+
+        System.out.println("Eu "+nomeCompleto +", morando no "+ endereçoString + " confirmo que recebi o salario de " + salarioDouble+ " na data "+ dataRecebimento);
+        
+    }
+}
